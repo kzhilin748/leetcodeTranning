4dfd9645dd5f35efc8abc552dd41f0e44294a5ab
kl <user@user.local>
2019-06-16 23:39:27 +0800

    13.Roman_to_Integer (use map)

--- 13Roman_to_Integer/Roman_to_Integer.cpp
+++ 13Roman_to_Integer/Roman_to_Integer.cpp
@@ -1,67 +1,39 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        
-        int totalnum=0;
-        int stringSize=s.size()-1;
-        while(stringSize>=0){
-            
-            if(s[stringSize]=='I'){
-                totalnum+=1;
-            }   
-            if(s[stringSize]=='V'){
-                totalnum+=5;
-                if((stringSize-1)>=0 && s[stringSize-1]=='I'){
-                    totalnum-=1;
-                    stringSize--;
-                }
-                    
-            }
-            else if(s[stringSize]=='X'){
-                totalnum+=10;
-                if((stringSize-1)>=0 &&s[stringSize-1]=='I'){
-                    totalnum-=1;
-                    stringSize--;
-                }
-                    
-            }
-            else if(s[stringSize]=='L'){
-                totalnum+=50;
-                if((stringSize-1)>=0 &&s[stringSize-1]=='X'){
-                    totalnum-=10;
-                    stringSize--;
-                }
-                    
-            }
-             else if(s[stringSize]=='C'){
-                totalnum+=100;
-                if((stringSize-1)>=0 &&s[stringSize-1]=='X'){
-                    totalnum-=10;
-                    stringSize--;
-                }
-                    
-            }           
-            else if(s[stringSize]=='D'){
-                totalnum+=500;
-                if((stringSize-1)>=0 &&s[stringSize-1]=='C'){
-                    totalnum-=100;
-                    stringSize--;
-                }
-                    
-            }
-            else if(s[stringSize]=='M'){
-                totalnum+=1000;
-                if((stringSize-1)>=0 && s[stringSize-1]=='C'){
-                    totalnum-=100;
-                    stringSize--;
-                }
-                    
-            } 
- //           cout<< totalnum <<"  " <<stringSize<<"  "<< s[stringSize]<<endl;
-            stringSize--;
+       map<char,int> romantrans={
+            {'I', 1}, 
+            {'V', 5}, 
+            {'X', 10}, 
+            {'L', 50},
+            {'C', 100}, 
+            {'D', 500}, 
+            {'M', 1000}
+       };
+       int totalnum=0;
+       int count=s.size()-1;
+       
+       try{
+           
+           while(count>-1){
+               if(romantrans.find(s[count])==romantrans.end())
+                   throw 0;
+               else if((romantrans.find(s[count]))->second >(romantrans.find(s[count-1]))->second){
+                    totalnum+= (((romantrans.find(s[count]))->second) - ((romantrans.find(s[count-1]))->second));   
+                    count--;
+               }
+               else {
+                   totalnum+=romantrans[s[count]];
+               }
+               count--;
+           }
 
-        } 
-        return totalnum;
+           
+       }
+       catch(int err){
+               cout<<"err"<<endl;
+       }
+        
+    return totalnum;
     }
 };
-
