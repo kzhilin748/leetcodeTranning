d4260c9ef3cd27a33c362bd31a2038d9f8c60ca6
Mywindows <My>
2019-07-07 19:22:19 +0800

    349 350

---
+++ 349_Intersection_of_Two_Arrays/349_Intersection_of_Two_Arrays.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        std::vector<int> Rvector;
+        if(nums1.empty()||nums2.empty()){
+            return Rvector;
+        }
+        mysort(nums1);mysort(nums2);
+        std::vector<int>::iterator it1=nums1.begin();
+        std::vector<int>::iterator it2=nums2.begin();
+        while(it1!=nums1.end()&&it2!=nums2.end()){
+            if(*it1==*it2){
+                if(Rvector.empty()||Rvector.back()!=(*it1)){
+                    Rvector.push_back(*it1);
+                }
+                it1++;it2++;
+
+            }
+            else if(*it1>*it2){
+                it2++;
+            }
+            else{
+                it1++;
+            }
+        }
+        return Rvector;
+    }
+private:
+    void mysort (vector<int>& list){
+        std::sort(list.begin(),list.end());
+    }
+};
