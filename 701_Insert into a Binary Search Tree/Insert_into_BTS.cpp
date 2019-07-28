c7776c3623c94b98a716567498861ddf3b867d62
Mywindows <My>
2019-07-28 10:53:20 +0800

    701

---
+++ 701_Insert into a Binary Search Tree/Insert_into_BTS.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* insertIntoBST(TreeNode* root, int val) {
+        return  findposition(root,&val);
+    }
+    TreeNode* findposition(TreeNode* root, int *val){
+        if(root==NULL){
+            return new TreeNode(*val);
+        }
+        if(root->val>(*val))
+            root->left=findposition(root->left,val);
+        else
+            root->right=findposition(root->right,val);
+        
+        return root;
+    }
+    
+
+};
