c7caef3a649474473cc70ec0e1064d7c78e974ee
kl <user@user.local>
2019-07-28 21:18:20 +0800

    701 update

--- 701_Insert into a Binary Search Tree/Insert_into_BTS.cpp
+++ 701_Insert into a Binary Search Tree/Insert_into_BTS.cpp
@@ -10,19 +10,30 @@
 class Solution {
 public:
     TreeNode* insertIntoBST(TreeNode* root, int val) {
-        return  findposition(root,&val);
-    }
-    TreeNode* findposition(TreeNode* root, int *val){
-        if(root==NULL){
-            return new TreeNode(*val);
+        TreeNode** node=&root;
+        while(*node!= NULL) {
+            node=(((*node)->val)>val)?(&((*node)->left)):(&((*node)->right));
         }
-        if(root->val>(*val))
-            root->left=findposition(root->left,val);
-        else
-            root->right=findposition(root->right,val);
-        
+        *node=new TreeNode(val);
         return root;
     }
+};
+// class Solution {
+// public:
+//     TreeNode* insertIntoBST(TreeNode* root, int val) {
+//         return  findposition(root,&val);
+//     }
+//     TreeNode* findposition(TreeNode* root, int *val){
+//         if(root==NULL){
+//             return new TreeNode(*val);
+//         }
+//         if(root->val>(*val))
+//             root->left=findposition(root->left,val);
+//         else
+//             root->right=findposition(root->right,val);
+        
+//         return root;
+//     }
     
 
-};
+// };
